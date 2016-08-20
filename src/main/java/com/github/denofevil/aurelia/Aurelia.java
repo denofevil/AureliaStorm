d8881640313d6905cf327925d5491d9da16ec41a
Dennis Ushakov <dennis.ushakov@gmail.com>
2016-08-20 22:33:37 +0300

    initial

---
+++ src/main/java/com/github/denofevil/aurelia/Aurelia.java
@@ -0,0 +1,12 @@
+package com.github.denofevil.aurelia;
+
+import com.intellij.openapi.project.Project;
+
+/**
+ * @author Dennis.Ushakov
+ */
+public class Aurelia {
+    public static boolean present(Project project) {
+        return true;
+    }
+}
