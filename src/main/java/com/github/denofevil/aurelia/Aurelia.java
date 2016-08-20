15f659f541d91c259c638fc3e4b9359791738423
Dennis Ushakov <dennis.ushakov@gmail.com>
2016-08-20 23:04:17 +0300

    a little bit of detection

--- src/main/java/com/github/denofevil/aurelia/Aurelia.java
+++ src/main/java/com/github/denofevil/aurelia/Aurelia.java
@@ -1,12 +1,14 @@
 package com.github.denofevil.aurelia;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
 
 /**
  * @author Dennis.Ushakov
  */
 public class Aurelia {
     public static boolean present(Project project) {
-        return true;
+        return FilenameIndex.getFilesByName(project, "aurelia-core.js", GlobalSearchScope.allScope(project)).length > 0;
     }
 }
