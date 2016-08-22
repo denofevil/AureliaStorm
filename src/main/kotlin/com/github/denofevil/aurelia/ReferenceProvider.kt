fc1959db673f09a1bb1fdce01677a6a0af41126f
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-08-22 23:17:40 +0200

    ReferenceProvider: Convert to Kotlin

---
+++ src/main/kotlin/com/github/denofevil/aurelia/ReferenceProvider.kt
@@ -0,0 +1,28 @@
+package com.github.denofevil.aurelia
+
+import com.intellij.lang.ecmascript6.resolve.ES6PsiUtil
+import com.intellij.psi.ElementManipulators
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import com.intellij.psi.PsiReferenceProvider
+import com.intellij.psi.xml.XmlAttribute
+import com.intellij.psi.xml.XmlAttributeValue
+import com.intellij.util.ProcessingContext
+
+/**
+ * @author Dennis.Ushakov
+ */
+class ReferenceProvider : PsiReferenceProvider() {
+    override fun getReferencesByElement(psiElement: PsiElement, processingContext: ProcessingContext): Array<PsiReference> {
+        val parent = psiElement.parent
+        if (psiElement is XmlAttributeValue && parent is XmlAttribute) {
+            val attrName = parent.name.removePrefix("data-")
+            if (Aurelia.AURELIA_APP == attrName) {
+                val text = ElementManipulators.getValueText(psiElement)
+                val range = ElementManipulators.getValueTextRange(psiElement)
+                return ES6PsiUtil.createImportExportFromClauseReferences(psiElement, range.startOffset, text, this)
+            }
+        }
+        return PsiReference.EMPTY_ARRAY
+    }
+}
