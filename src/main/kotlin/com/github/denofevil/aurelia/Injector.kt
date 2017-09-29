0582b4992f40bbcdf4cd63951f0f94f8b2d06e59
Dennis Ushakov <dennis.ushakov@gmail.com>
2017-09-29 18:32:58 +0300

    fix injection into comments (fixes #21, #27)

--- src/main/kotlin/com/github/denofevil/aurelia/Injector.kt
+++ src/main/kotlin/com/github/denofevil/aurelia/Injector.kt
@@ -7,12 +7,15 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.ElementManipulators
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiLanguageInjectionHost
+import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.impl.source.xml.XmlAttributeValueImpl
 import com.intellij.psi.impl.source.xml.XmlTextImpl
+import com.intellij.psi.templateLanguages.OuterLanguageElement
 import com.intellij.psi.xml.XmlAttribute
 import com.intellij.psi.xml.XmlAttributeValue
-
-import java.util.Arrays
+import com.intellij.psi.xml.XmlElementType
+import com.intellij.psi.xml.XmlTokenType
+import java.util.*
 
 /**
  * @author Dennis.Ushakov
@@ -41,10 +44,20 @@ class Injector : MultiHostInjector {
         while (start >= 0) {
             var end = text.indexOf("}", start)
             end = if (end >= 0) end else range.length
-            registrar.startInjecting(JavascriptLanguage.INSTANCE)
-                    .addPlace(null, null, host as PsiLanguageInjectionHost,
-                            TextRange(range.startOffset + start + 2, range.startOffset + end))
-                    .doneInjecting()
+            var injectionCandidate = host.findElementAt(start)
+            while (injectionCandidate is PsiWhiteSpace) injectionCandidate = injectionCandidate.getNextSibling()
+
+            if (injectionCandidate != null &&
+                    injectionCandidate.startOffsetInParent <= end &&
+                    !XmlTokenType.COMMENTS.contains(injectionCandidate.node.elementType) &&
+                    injectionCandidate.node.elementType !== XmlElementType.XML_COMMENT &&
+                    injectionCandidate !is OuterLanguageElement) {
+
+                registrar.startInjecting(JavascriptLanguage.INSTANCE)
+                        .addPlace(null, null, host as PsiLanguageInjectionHost,
+                                TextRange(range.startOffset + start + 2, range.startOffset + end))
+                        .doneInjecting()
+            }
             start = text.indexOf("\${", end)
         }
     }
