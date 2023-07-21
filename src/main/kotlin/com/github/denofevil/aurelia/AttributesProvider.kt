9fc681faca124ef2d49ce948a8544f3cbe3d6ca1
Andrey Starovoyt <Andrey.Starovoyt@jetbrains.com>
2023-07-21 18:17:35 +0300

    2023.2 compatibility

--- src/main/kotlin/com/github/denofevil/aurelia/AttributesProvider.kt
+++ src/main/kotlin/com/github/denofevil/aurelia/AttributesProvider.kt
@@ -51,7 +51,6 @@ class AttributesProvider : XmlAttributeDescriptorsProvider {
         override fun isEnumerated(): Boolean = false
         override fun getDeclaration(): PsiElement? = null
         override fun getName(): String = name
-        override fun getDependences(): Array<Any> = ArrayUtil.EMPTY_OBJECT_ARRAY
         override fun isFixed(): Boolean = false
         override fun getDefaultValue(): String? = null
         override fun getEnumeratedValues(): Array<String>? = ArrayUtil.EMPTY_STRING_ARRAY
